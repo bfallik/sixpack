390ec193ed575421fda624a552667f1ada2c1672
Brian Fallik <bfallik@gmail.com>
2014-03-15 16:06:57 -0400

    initial hello (beer) world
    This code is based on
    https://developers.google.com/appengine/docs/go/gettingstarted/helloworld.

---
+++ aleager/aleager.go
@@ -0,0 +1,14 @@
+package aleager
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, beer world!")
+}
