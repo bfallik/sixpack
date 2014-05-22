af3f9c015fd2547e03c5bc6556cc280b0a0ea0b4
Brian Fallik <brian@clypd.com>
2014-05-21 23:40:35 -0400

    rm console.log()

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -4,7 +4,6 @@ searchApp.controller('searchCtrl', function ($scope, $resource) {
 	$scope.doSearch = function(query) {
 	var beerSearch = $resource("/api/untappd/noauth/search/beer", {});
 	beerSearch.get({"q": query}).$promise.then(function(beers) {
-		console.log(beers);
 		$scope.beers = beers;
 	})};
 
